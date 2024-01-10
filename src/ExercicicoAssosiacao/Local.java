773032659fe5ceefca60bcd5c6d400428e153813
Bruno Sales da Silva <bruno.sales.silva17@gmail.com>
2024-01-10 12:26:18 -0300

    10.01.24

---
+++ src/ExercicicoAssosiacao/Local.java
@@ -0,0 +1,26 @@
+package ExercicicoAssosiacao;
+
+public class Local {
+    private String local;
+    Seminario [] seminarios;
+
+    public Local(String local) {
+        this.local = local;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public Seminario[] getSeminarios() {
+        return seminarios;
+    }
+
+    public void setSeminarios(Seminario[] seminarios) {
+        this.seminarios = seminarios;
+    }
+}
---
+++ src/ExercicicoAssosiacao/Seminario.java
@@ -0,0 +1,46 @@
+package ExercicicoAssosiacao;
+
+public class Seminario {
+    private String titulo;
+    private Aluno [] alunos;
+
+    private Professor professor;
+
+    private Local local;
+
+    public Seminario(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Aluno[] getAlunos() {
+        return alunos;
+    }
+
+    public void setAlunos(Aluno[] alunos) {
+        this.alunos = alunos;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(Professor professor) {
+        this.professor = professor;
+    }
+
+    public Local getLocal() {
+        return local;
+    }
+
+    public void setLocal(Local local) {
+        this.local = local;
+    }
+}
