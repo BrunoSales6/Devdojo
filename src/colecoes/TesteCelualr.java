42ab26ef21ac9a44847a9cd9de3e8e8033c3a17e
Bruno Sales da Silva <bruno.sales.silva17@gmail.com>
2024-06-25 13:28:36 -0300

    025.06.2024

--- src/colecoes/TesteCelualr.java
+++
@@ -1,9 +0,0 @@
-package colecoes;
-
-public class TesteCelualr {
-    public static void main(String[] args) {
-        Celular celular=new Celular("Iphone","1234");
-        Celular celular2=new Celular("IphoneX","1234");
-        System.out.println(celular2.equals(celular));
-    }
-}
